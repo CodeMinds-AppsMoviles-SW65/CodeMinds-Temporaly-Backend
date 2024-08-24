e91a2064150e3dd22e66adb09491f53b8db5fb7c
Alex Avila <alex.ramon2004@gmail.com>
2024-08-23 23:25:28 -0500

    chore: init project.

---
+++ src/main/java/git/codeminds/temporaly/TemporalyBackendApplication.java
@@ -0,0 +1,13 @@
+package git.codeminds.temporaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemporalyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/git/codeminds/temporaly/TemporalyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package git.codeminds.temporaly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
