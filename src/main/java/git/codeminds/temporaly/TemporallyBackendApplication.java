2cc8c6330aee41660a1088f63859544abccd4d18
Alex Avila <alex.ramon2004@gmail.com>
2024-08-23 23:32:57 -0500

    feat(profiles): added spring boot profiles.

--- src/main/java/git/codeminds/temporaly/TemporallyBackendApplication.java
+++ src/main/java/git/codeminds/temporaly/TemporallyBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemporalyBackendApplication {
+public class TemporallyBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TemporalyBackendApplication.class, args);
+        SpringApplication.run(TemporallyBackendApplication.class, args);
     }
 
 }
